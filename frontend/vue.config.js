9e71ca069a9f679372ed49b2eeb9c3a54b0ab6a4
Charlie Vieillard <charlie.vieillard@endeavour.nl>
2026-08-28 12:49:52 +0200

    Configure Vue dev-server to accept Playwright e2e tests.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: (process.env.DEV_SERVER_ALLOWED_HOSTS || 'localhost').split(
+      ',',
+    ),
+  },
 })

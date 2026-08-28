c81ad7df13d86f34aca2a3e035ad5f1d7377fd45
Charlie Vieillard <charlie.vieillard@endeavour.nl>
2026-08-28 13:12:46 +0200

    New Vue component (detail.vue) showing cocktail details.

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,6 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import CocktailList from '@/components/cocktails/list.vue';
 import CocktailNew from '@/components/cocktails/new.vue';
+import CocktailDetail from '@/components/cocktails/detail.vue';
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     path: '/cocktails/new',
     name: 'cocktail_new',
     component: CocktailNew,
+  },
+  {
+    path: '/cocktails/:id',
+    name: 'cocktail_detail',
+    component: CocktailDetail,
   },
 ];
 

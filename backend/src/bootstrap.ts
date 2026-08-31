d83d6cb51e645564e5e75ce7b14fef432e3e835d
Charlie Vieillard <charlie.vieillard@endeavour.nl>
2026-08-31 13:59:02 +0200

    New IsTitleUniqueConstraint to validate Cocktail.title.

--- backend/src/bootstrap.ts
+++ backend/src/bootstrap.ts
@@ -1,8 +1,15 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 
 export function configureApp(app: INestApplication) {
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 }
